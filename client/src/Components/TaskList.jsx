eac26f718c7ba93e42afe04ae53fe68d9d03fe27
Brayan <puertacuartasbrayan2000@gmail.com>
2025-05-11 17:59:23 -0500

    se crean el archivo de dependencias y el gitignore

--- client/src/Components/TaskList.jsx
+++ client/src/Components/TaskList.jsx
@@ -1,12 +1,13 @@
 import { useEffect } from "react";
-import {getAllTasks} from '../api/tasks.api'
+import getAllTasks from "../api/tasks.api";
 function TaskList(){
 
     useEffect(() => {
-        function loadTasks(){
-            const res = getAllTasks();
+        async function loadTasks(){
+            const res = await getAllTasks();
             console.log(res);
         }
+        loadTasks()
     },[])
 
     return(
--- django_crud_api/settings.py
+++ django_crud_api/settings.py
@@ -129,6 +129,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #cors autorizados
-CORS_ALLOWED_ORIGINS = []
-
+#CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "http://localhost:3000"]
+CORS_ALLOWED_ORIGINS = True
   
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
+}
