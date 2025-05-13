0d50133599afb0f84953314aa0a9bff64550a031
Brayan <puertacuartasbrayan2000@gmail.com>
2025-05-12 23:39:06 -0500

    more changes

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 
-const getAllTasks = async () => {
+export const getAllTasks =  () => {
 
     return axios.get('http://localhost:8000/tasks/api/v1/tasks/')
 
 }
 
 export default getAllTasks;
+
+
 
--- client/src/pages/TaskFormPage.jsx
+++ client/src/pages/TaskFormPage.jsx
@@ -1,8 +1,32 @@
+import {useForm} from "react-hook-form";
+import Post from '../api/postAPI'
+
 export default function TaskFormPage() {
+
+    const {register, handleSubmit}= useForm();
+
+    const onSubmit = handleSubmit( async data => {
+        const respuesta = await Post(data)
+        console.log(respuesta);
+    });
+
+
     return(
        <>
-        <div style={{color: 'white'}}>
-            <h1>TaskFormPage</h1>
+        <div>
+            <form  onSubmit={onSubmit} >
+                <input type="text" 
+                placeholder="title"
+                {...register("titulo", {required: true})}
+                />
+
+                <textarea
+                rows='3'
+                placeholder="descripcion"
+                {...register('descripcion', {required: true})}/>
+
+                <button type="submit">save</button>
+            </form>
         </div>
        </> 
     )
